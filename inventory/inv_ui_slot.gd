fe560bb582030878d72f10cc489e98a441e55335
Untitled-Productions <bowerj7275@gmail.com>
2025-07-29 20:59:29 -0400

    still broken

--- inventory/inv_ui_slot.gd
+++ inventory/inv_ui_slot.gd
@@ -5,14 +5,12 @@ extends Panel
 
 @onready var item_visual: Sprite2D = $CenterContainer/Panel/item_display
 @onready var amount_text: Label = $CenterContainer/Panel/Label
-@onready var above = false
 @onready var grabbed = false
 
 func visuals(slot: InvSlot):
 	if !slot.item:
 		item_visual.visible = false
 		amount_text.visible = false
-
 	else:
 		item_visual.visible = true
 		item_visual.texture = slot.item.texture
@@ -34,11 +32,15 @@ func _on_button_pressed():
 			Global.mouseani.sprite_frames.remove_frame("item", 0)
 			Global.mouseani.sprite_frames.add_frame("item", slot.item.texture)
 			Global.mouseani.item = slot.item
+			if slot.amount > 1:
+				Global.mouseani.mouse_amount.visible = true
+				Global.mouseani.mouse_amount.text = str(slot.amount)
 			grabbed = true
 		if slot.amount > 0 and grabbed == true:
-			if slot.amount >= 1:
+			if slot.amount > 1:
 				slot.amount = max(slot.amount - 1, 0)
 			grabbed = false
+			Global.mouseani.mouse_amount.visible = false
 			visuals(slot)
 		if slot.amount == 0:
 			slot.item.texture = null
@@ -53,9 +55,9 @@ func _on_button_pressed():
 		slot.amount += 1
 		Global.mouseani.item = null
 		Global.mouseani.sprite_frames.remove_frame("item", 0)
-		above = true
 		visuals(slot)
-	if slot.amount > 0 and above == true and grabbed == true:
+	if slot.amount > 0 and grabbed == true:
+		slot.amount += 1
 		Global.mouseani.item = null
 		Global.mouseani.sprite_frames.remove_frame("item", 0)
 		visuals(slot)
--- mouse_anim.gd
+++ mouse_anim.gd
@@ -1,6 +1,8 @@
 extends AnimatedSprite2D
 
+@onready var mouse_amount: Label = $Label
 @export var item: InvItem
+@export var amount: InvSlot
 
 func _ready():
 	Global.mouseani = self
@@ -9,4 +11,4 @@ func _ready():
 	SpriteFrame.add_animation("item")
 	SpriteFrame.add_frame("item", item)
 	ani.play("item")
-	
+	Global.mouseani.mouse_amount.visible = false
